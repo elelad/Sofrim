fc81317b315ad69b186aa0acbedebd22107f030c
elelad <elelad.dev@gmail.com>
2020-03-26 19:38:22 +0200

    added navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NavController, PopoverController } from '@ionic/angular';
+import { SettingsService } from '../../services/settings.service';
+import { ShareMenuComponent } from '../share-menu/share-menu.component';
+import { AccMenuComponent } from '../acc-menu/acc-menu.component';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  @Input() navTitle = 'סופרים וזוכרים';
+  @Input() accBtn = true;
+  @Input() shareBtn = true;
+  @Input() menuBtn = true;
 
-  constructor() { }
+  constructor(public navCtrl: NavController, public settingsService: SettingsService, private popoverCtrl: PopoverController) { }
 
-  ngOnInit() {}
+  async presentPopover(myEvent) {
+    const popover = await this.popoverCtrl.create({
+      component: AccMenuComponent,
+      event: myEvent
+    });
+    popover.present();
+  }
+
+  async presentShaerPopover(myEvent) {
+    const popover = await this.popoverCtrl.create({
+      component: ShareMenuComponent,
+      event: myEvent
+    });
+    popover.present();
+  }
+
+
 
 }
+
